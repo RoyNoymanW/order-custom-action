5fec6f4cd0300d944ed73de6157bd6b26f9a8262
ronene <ronene@wix.com>
2024-09-17 16:54:36 +0300

    change the response to be and order

--- src/backend/api/orders/api.ts
+++ src/backend/api/orders/api.ts
@@ -4,5 +4,5 @@ import {getOrder} from "../../orders";
 export async function GET(req: Request) {
     const _id = new URL(req.url).searchParams.get('orderId') as string;
     const order = await getOrder(_id);
-    return new Response(JSON.stringify(order ?? {}));
+    return Response.json(order);
 }
