8a27576ca6b81047c0e718deb4342ccc2b25b4ae
ronene <ronene@wix.com>
2024-09-17 16:35:05 +0300

    add method to get order details, phone number and products

--- src/backend/orders.ts
+++ src/backend/orders.ts
@@ -1,18 +1,25 @@
 import {orders} from '@wix/ecom';
+import {OrderDetails} from "../types";
 
 export async function getOrder(_id: string): Promise<orders.Order> {
     //https://dev.wix.com/docs/sdk/backend-modules/ecom/orders/get-order
-    const order =  await orders.getOrder(_id);
+    const order = await orders.getOrder(_id);
     return order;
 }
 
 export function getBuyerPhoneNumber(order: orders.Order): string | null | undefined {
-    if(order.billingInfo) return order.billingInfo?.contactDetails?.phone;
-    else if(order.shippingInfo) return order.recipientInfo?.contactDetails?.phone;
-    else{
+    if (order.billingInfo) return order.billingInfo?.contactDetails?.phone;
+    else if (order.shippingInfo) return order.recipientInfo?.contactDetails?.phone;
+    else {
         console.log(`No phone number found in order ${JSON.stringify(order)}`);
         return null;
     }
+}
+
+export function getDetailsFromOrder(order: orders.Order): OrderDetails {
+    const phoneNumber: string | null | undefined = getBuyerPhoneNumber(order);
+    const orderProducts: (string | undefined)[] | undefined = order.lineItems?.map(lineItem => lineItem.productName?.original);
+    return {phoneNumber, orderProducts}
 }
 
 export function getBuyerLanguage(order: orders.Order): string | null | undefined {
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
+
 export type Settings = {
   amount: number;
   title: string;
   color: string;
   iconColor: string;
 };
+
+export type OrderDetails = {
+  phoneNumber: string | null | undefined;
+  orderProducts: (string | undefined)[] | undefined
+}
