45ac2439af738344b8ea3d0ada6b03c9b95c95f7
RoyNoymanW <royno@wix.com>
2024-09-18 13:58:27 +0300

    radio

--- src/dashboard/modals/whatup-modals/modal.tsx
+++ src/dashboard/modals/whatup-modals/modal.tsx
@@ -6,7 +6,7 @@ import {
     Box,
     Image,
     Button,
-    CustomModalLayout, Dropdown, InputArea, FormField, Divider,
+    CustomModalLayout, RadioGroup, Radio, Avatar, InputArea, FormField, Divider,
 } from '@wix/design-system';
 import '@wix/design-system/styles.global.css';
 import {generateWhatsappLink, generateWhatsappUpsellMessage} from '../../../utils/whatsapp-link-generator';
@@ -19,9 +19,9 @@ const Modal: FC<{ orderId: string }> = (props) => {
     const orderId = props.orderId;
     console.log('orderId:', orderId);
 
-    const [selectedProduct, setSelectedProduct] = useState<{id:string,value:string}>({id:"",value: ""});
+    const [selectedProduct, setSelectedProduct] = useState<{id:string,value:string,image:string}>({id:"",value: "",image:""});
     console.log("initial: ", selectedProduct)
-    const [productOptionsList, setProductOptionsList] = useState<{id:string,value:string}[]>([]);
+    const [productOptionsList, setProductOptionsList] = useState<{id:string,value:string,image:string}[]>([]);
     const [phoneNumber, setPhoneNumber] = useState<string | null | undefined>(undefined);
     const [contactName, setContactName] = useState<string | null | undefined>(undefined);
     const [controlledWhatsappMessage, setControlledWhatsappMessage] = useState('');
@@ -51,7 +51,8 @@ const Modal: FC<{ orderId: string }> = (props) => {
                 const response = await callGetOrderById()
                 const order = await response.json()
                 const details =  getDetailsFromOrder(order)
-                const mappedOptions = details.orderProducts?.map(productDetails => ({id: productDetails.catalogItemId!, value: productDetails.productName!}))
+                console.log("details.orderProducts useEffect: ", details.orderProducts)
+                const mappedOptions = details.orderProducts?.map(productDetails => ({id: productDetails.catalogItemId!, value: productDetails.productName!,image:productDetails.image!}))
                 setProductOptionsList(mappedOptions!)
                 setPhoneNumber(validateAndEditPhoneNumber(details.contactDetails?.phoneNumber))
                 setContactName(generateContactName(details.contactDetails?.firstName,details.contactDetails?.lastName))
@@ -83,17 +84,32 @@ const Modal: FC<{ orderId: string }> = (props) => {
                 subtitle="selcet products to resale"
                 content={
                     <Box direction="vertical" align="stretch" margin="medium">
-                        <Dropdown
-                            placeholder="Select a product"
-                            options={productOptionsList}
-                            selectedId={selectedProduct.id}
-                            onSelect={
-                                (option:{id:string,value:string}) => {
-                                    console.log("option:" ,option)
-                                    setSelectedProduct(option)
+                        <Text weight="bold">Select an item:</Text>
+                        <RadioGroup
+                            value={selectedProduct.id || ''}
+                            onChange={ (prodId) => {
+                                    const product = productOptionsList.find(p => {
+                                        console.log('prodId', prodId);
+                                        console.log('p.id', p.id);
+                                        return p.id === prodId;
+                                    });
+                                    console.log("product inside on change", product)
+                                    setSelectedProduct(product);
                                 }
                             }
-                        />
+                        >
+                            {productOptionsList.map((product) => {
+                                console.log("product is: ",product)
+                                return (<RadioGroup.Radio value={product.id}>
+                                    <Box gap={2} verticalAlign="middle">
+                                        <Image src={product.image} />,
+                                        <Box direction="vertical">
+                                            <Text weight="normal">{product.value}</Text>
+                                        </Box>
+                                    </Box>
+                                </RadioGroup.Radio>)
+                                })}
+                        </RadioGroup>
                         <Box marginTop={'medium'}>
                             <Divider skin="light" />
                         </Box>
@@ -134,5 +150,4 @@ const renderOrder = async (orderId: string) => {
         });
     };
 }
-
 export default Modal;
