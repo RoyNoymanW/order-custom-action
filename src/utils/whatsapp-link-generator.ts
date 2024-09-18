76fafb177ee7984d7b3faa45f9926a3a5c646ee8
ronene <ronene@wix.com>
2024-09-18 11:30:07 +0300

    update message and method params to whatsup generator

--- src/utils/whatsapp-link-generator.ts
+++ src/utils/whatsapp-link-generator.ts
@@ -1,6 +1,6 @@
 
-export const generateWhatsappLink = (contactName: string, productId: string, productName: string, couponCode: string, phone: string) => {
-    const message: string = generateWhatsappUpsellMessage(contactName, productId, productName, couponCode)
+export const generateWhatsappLink = (contactName: string, productId: string, productName: string, couponCode: string, phone: string, productLink: string) => {
+    const message: string = generateWhatsappUpsellMessage(contactName, productId, productName, couponCode, productLink)
     const whatsappLink = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
     console.log(whatsappLink)
     return whatsappLink
@@ -10,9 +10,8 @@ const generateProductLink = (productId: string) => {
     return 'https://www.example.com/products/' + productId;
 }
 
-const generateWhatsappUpsellMessage = (contactName: string, productId: string, productName: string, couponCode: string) => {
-    return `Hi ${contactName}, we want to offer you an upsell for ${productName} for 50% off with this coupon code: ${couponCode}. 
-    Click here to purchase: ${generateProductLink(productId)}`;
+const generateWhatsappUpsellMessage = (contactName: string, productId: string, productName: string, couponCode: string, productLink: string) => {
+    return `Hey ${contactName}!\nHope you're still enjoying your ${productName}! :blush: Need a new one?\n 
+    Grab it now and get 10% OFF with the code ${couponCode}. Just tap here to order: ${productLink}.
+    Don't miss out!
 }
-
-
